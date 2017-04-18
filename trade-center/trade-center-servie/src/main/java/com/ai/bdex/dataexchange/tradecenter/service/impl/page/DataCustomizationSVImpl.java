755bec4b5c737bbd401b53d143f24c86780ea30e
landeng <landeng@asiainfo.com>
2017-04-18 20:01:29 +0800

    生成数据定制

--- trade-center/trade-center-servie/src/main/java/com/ai/bdex/dataexchange/tradecenter/service/impl/page/DataCustomizationSVImpl.java
+++ trade-center/trade-center-servie/src/main/java/com/ai/bdex/dataexchange/tradecenter/service/impl/page/DataCustomizationSVImpl.java
@@ -1,5 +1,26 @@
 package com.ai.bdex.dataexchange.tradecenter.service.impl.page;
 
-public class DataCustomizationSVImpl {
+import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
+import com.ai.bdex.dataexchange.tradecenter.dao.mapper.DataCustomizationMapper;
+import com.ai.bdex.dataexchange.tradecenter.dao.model.DataCustomization;
+import com.ai.bdex.dataexchange.tradecenter.service.interfaces.page.IDataCustomizationSV;
+ 
+@Service("iDataCustomizationSV")
+public class DataCustomizationSVImpl implements IDataCustomizationSV {
+	  @Resource
+	  private DataCustomizationMapper dataCustomizationMapper;
+	   /***
+	    * 接口没有时，需要定制的信息
+	    */
+	  @Override
+	    public DataCustomization queryDATAById(Integer dcza_id) throws Exception {
+ 	        if (dcza_id==null){
+	            throw new Exception("根据ID查询 数据定制 信息入参为空");
+	        }
+ 	       DataCustomization dataCustomizationSVImpl =  dataCustomizationMapper.selectByPrimaryKey(dcza_id);
+	        return dataCustomizationSVImpl;
+	    }
 }
--- trade-center/trade-center-servie/src/main/java/com/ai/bdex/dataexchange/tradecenter/service/interfaces/page/IDataCustomizationSV.java
+++ trade-center/trade-center-servie/src/main/java/com/ai/bdex/dataexchange/tradecenter/service/interfaces/page/IDataCustomizationSV.java
@@ -1,5 +1,7 @@
 package com.ai.bdex.dataexchange.tradecenter.service.interfaces.page;
 
+import com.ai.bdex.dataexchange.tradecenter.dao.model.DataCustomization;
+ 
 public interface IDataCustomizationSV {
-
+	   public DataCustomization queryDATAById(Integer dcza_id) throws Exception;
 }
