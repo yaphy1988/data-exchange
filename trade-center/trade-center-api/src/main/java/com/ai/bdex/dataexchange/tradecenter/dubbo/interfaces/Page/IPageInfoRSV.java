1334edc84e03af8c1ae6a23278a35f30b1058c77
landeng <landeng@asiainfo.com>
2017-04-19 16:23:23 +0800

    解决一个首页接口错误

--- trade-center/trade-center-api/src/main/java/com/ai/bdex/dataexchange/tradecenter/dubbo/interfaces/Page/IPageInfoRSV.java
+++ trade-center/trade-center-api/src/main/java/com/ai/bdex/dataexchange/tradecenter/dubbo/interfaces/Page/IPageInfoRSV.java
@@ -8,13 +8,5 @@ import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.Gds.GdsInfoReqDTO;
 import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.Gds.GdsInfoRespDTO;
 
 public class IPageInfoRSV {
-    public PageInfoRespDTO queryGdsInfoDetails (GdsInfoReqDTO gdsInfoReqDTO) throws Exception;
-    
-    /**
-     * 查询商品分类
-     * @param gdsCatReqDTO
-     * @return
-     * @throws Exception
-     */
-    public List<GdsCatRespDTO> queryGdsCatListDTO(GdsCatReqDTO gdsCatReqDTO) throws Exception;
+ 
 }
