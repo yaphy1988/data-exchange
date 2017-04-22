03ef40874212a2f5df5d907ff657c965c12f65b6
landeng <landeng@asiainfo.com>
2017-04-22 09:55:43 +0800

    冲突解决

--- trade-center/trade-center-servie/src/main/java/com/ai/bdex/dataexchange/tradecenter/dubbo/impl/gds/GdsCatRSVImpl.java
+++ trade-center/trade-center-servie/src/main/java/com/ai/bdex/dataexchange/tradecenter/dubbo/impl/gds/GdsCatRSVImpl.java
@@ -8,6 +8,9 @@ import com.ai.bdex.dataexchange.tradecenter.service.interfaces.gds.IGdsCatSV;
 
 import com.ai.bdex.dataexchange.util.ObjectCopyUtil;
 import com.ai.paas.utils.CollectionUtil;
+import com.alibaba.dubbo.common.utils.CollectionUtils;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -49,32 +52,22 @@ public class GdsCatRSVImpl implements IGdsCatRSV {
     @Override
     public List<GdsCatRespDTO> queryGdsCatList(GdsCatReqDTO gdsCatReqDTO) throws Exception {
         List<GdsCatRespDTO> respDTOList = new ArrayList<GdsCatRespDTO>();
-        try{
+       try{
         	 if (gdsCatReqDTO ==null || gdsCatReqDTO.getCatPid()==null){
                  throw new Exception("查询商品分类信息异常，入参为空");
              }
-             List<GdsCat> catList= iGdsCatSV.queryGdsCatList(gdsCatReqDTO);
-<<<<<<< Upstream, based on origin/develop
-             if(!CollectionUtil.isEmpty(catList)){
-            	 GdsCatRespDTO respDTO = new GdsCatRespDTO();
-=======
-             if(CollectionUtils.isNotEmpty(catList)){
->>>>>>> 6229a3c 商品分类
-            	 for(GdsCat catVO:catList){
-<<<<<<< Upstream, based on origin/develop
-                     ObjectCopyUtil.copyObjValue(catVO,respDTO,null,false);
-//            		 BeanUtils.copyProperties(catVO,respDTO);
-=======
-                	 GdsCatRespDTO respDTO = new GdsCatRespDTO();
-            		 BeanUtils.copyProperties(catVO,respDTO);
->>>>>>> 6229a3c 商品分类
+             List<GdsCat> catList= iGdsCatSV.queryGdsCatList(gdsCatReqDTO); 
+             if(CollectionUtils.isNotEmpty(catList)){ 
+            	 for(GdsCat catVO:catList){ 
+                 	 GdsCatRespDTO respDTO = new GdsCatRespDTO();
+            		 BeanUtils.copyProperties(catVO,respDTO); 
             		 respDTOList.add(respDTO);
             	 }
              }
         }catch(Exception e){
         	log.error("获取商品分类信息异常:", e);
             throw new Exception(e);
-        }
+        } 
         return respDTOList;
     }
 }
