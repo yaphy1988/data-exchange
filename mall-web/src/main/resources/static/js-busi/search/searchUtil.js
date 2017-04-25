5474b25fdc8a4b8de9113e8b02376d97ca0ebee4
gongxq <gongxq@asiainfo.com>
2017-04-25 14:56:43 +0800

    联想

---
+++ mall-web/src/main/resources/static/js-busi/search/searchUtil.js
@@ -0,0 +1,65 @@
+$(function(){
+	if($('#siteSearch').size()>0){
+        $(document).on('click.hideAutocomplete',function () {
+            var event = event || window.event;
+            var target = event.target || event.srcElement;
+            if($(target).parents('.autocomplete-container').size()<=0 && !$(target).hasClass('autocomplete-container')){
+                $('.autocomplete-container .proposal-list').empty();
+            }
+        });
+        $('#siteSearch').autocomplete({
+                //        hints: words,
+                placeholder: 'Search',
+                width: 373,
+                height: 25,
+                showButton: true,
+                buttonText: '搜索',
+                onSubmit:function(input){
+                    alert(input.val());
+                    //window.location.href='https://www.baidu.com/'
+                },
+                inputSet:function(input,currentProposals,currentSelection){
+                    if(currentSelection>-1){
+                        var curPro=currentProposals[currentSelection];
+                        input.val(curPro.val);
+                        input.attr('sub-id',curPro.id);
+                    }
+
+                },
+                onChange:function(params,input,proposalList,currentProposals){
+                    var vals=[{id:'1',val:'衣服'},{id:'2',val:'裤子'},{id:'3',val:'裤子'},{id:'4',val:'裤子'},{id:'5',val:'裤子'}
+                        ,{id:'6',val:'裤子'},{id:'7',val:'裤子'},{id:'8',val:'裤子'}];
+                    for(var index in vals){
+                        var v=vals[index];
+                        currentProposals.push({id: v.id,val:v.val});
+                        var element = $('<li></li>')
+                            .addClass('proposal')
+                            .click(function(){
+                                params.inputSet(input,currentProposals,index);
+                                proposalList.empty();
+                                params.onSubmit(input);
+                            })
+                            .mouseenter(function() {
+                                $(this).addClass('selected');
+                            })
+                            .mouseleave(function() {
+                                $(this).removeClass('selected');
+                            });
+
+                        var html= "<span class='search-val'>"+v.val+"</span>" +
+                            "<span class='search-num'>搜索约1000个结果</span>";
+                        if(index>proposalList.find('li').size()-1){
+                            element.html(html);
+                            proposalList.append(element);
+                        }else{
+                            proposalList.find('li').eq(index).html(html);
+                        }
+                    }
+                    if(vals.length<proposalList.size()){
+                        proposalList.find('li:gt('+(vals.length-1)+')').remove();
+                    }
+                }
+            }
+        );
+    }
+});
---
+++ mall-web/src/main/resources/static/js/autocomplete/autocomplete.js
@@ -0,0 +1,190 @@
+/**
+A jQuery plugin for search hints
+
+Author: Lorenzo Cioni - https://github.com/lorecioni
+*/
+
+(function($) {
+	$.fn.autocomplete = function(params) {
+		//Selections
+		var currentSelection = -1;
+		var currentProposals = [];
+		
+		//Default parameters
+		params = $.extend({
+			hints: [],
+			placeholder: 'Search',
+			width: 200,
+			height: 16,
+            maxlength:255,
+			showButton: true,
+			buttonText: 'Search',
+			inputSet:null,
+			onSubmit: function(text){},
+			onBlur: function(){},
+			onChange:null
+		}, params);
+
+		//Build messagess
+		this.each(function() {
+			//Container
+			var searchContainer = $('<div></div>')
+				.addClass('autocomplete-container')
+				.css('height', params.height * 2);	
+				
+			//Text input		
+			var input = $('<input type="text" id="searchTxt" autocomplete="off" name="query">')
+				.attr('placeholder', params.placeholder)
+				.attr('maxlength', params.maxlength)
+				.addClass('autocomplete-input')
+				.css({
+					'width' : params.width,
+					'height' : params.height
+				});
+			
+			if(params.showButton){
+				input.css('border-radius', '3px 0 0 3px');
+			}
+
+			//Proposals
+			var proposals = $('<div></div>')
+				.addClass('proposal-box')
+				.css('width', params.width + 18)
+				.css('top', input.height() + 20);
+			var proposalList = $('<ul></ul>')
+				.addClass('proposal-list');
+
+			proposals.append(proposalList);
+			input.keydown(function(e) {
+				switch(e.which) {
+					case 38: // Up arrow
+					e.preventDefault();
+					$('ul.proposal-list li').removeClass('selected');
+					if((currentSelection - 1) >= 0){
+						currentSelection--;
+						$( "ul.proposal-list li:eq(" + currentSelection + ")" )
+							.addClass('selected');
+						/* zqr ������������ */
+						if(params.inputSet){
+							params.inputSet(input,currentProposals,currentSelection);
+						}
+					} else {
+						currentSelection = -1;
+					}
+					break;
+					case 40: // Down arrow
+					e.preventDefault();
+					if((currentSelection + 1) < currentProposals.length){
+						$('ul.proposal-list li').removeClass('selected');
+						currentSelection++;
+						$( "ul.proposal-list li:eq(" + currentSelection + ")" )
+							.addClass('selected');
+						/* zqr ������������ */
+						if(params.inputSet){
+							params.inputSet(input,currentProposals,currentSelection);
+						}
+					}
+					break;
+					case 13: // Enter
+						if(params.inputSet){
+							params.inputSet(input,currentProposals,currentSelection);
+						}else{
+							if(currentSelection > -1){
+								var text = $( "ul.proposal-list li:eq(" + currentSelection + ")" ).html();
+								input.val(text);
+							}
+						}
+						currentSelection = -1;
+						proposalList.empty();
+						params.onSubmit(input);
+						break;
+					case 27: // Esc button
+						currentSelection = -1;
+						proposalList.empty();
+						input.val('');
+						break;
+				}
+			});
+			var prevVal='';
+			input.bind("change paste keyup", function(e){
+				if(e.which != 13 && e.which != 27 
+						&& e.which != 38 && e.which != 40){				
+					currentProposals = [];
+					currentSelection = -1;
+					if(prevVal==$(this).val()){
+				    	return true;
+					}
+					prevVal=$(this).val();
+					/*** zqr �޸� **/
+					if(params.onChange){
+						params.onChange(params,$(this),proposalList,currentProposals)
+					}
+					else if(input.val() != ''){
+						proposalList.empty();
+						var word = "^" + input.val() + ".*";
+						for(var test in params.hints){
+							if(params.hints[test].match(word)){
+								currentProposals.push(params.hints[test]);
+								var element = $('<li></li>')
+									.html(params.hints[test])
+									.addClass('proposal')
+									.click(function(){
+										input.val($(this).html());
+										proposalList.empty();
+										params.onSubmit(input.val());
+									})
+									.mouseenter(function() {
+										$(this).addClass('selected');
+									})
+									.mouseleave(function() {
+										$(this).removeClass('selected');
+									});
+							    	proposalList.append(element);
+							}
+						}
+
+					}
+				}
+			});
+			
+			input.blur(function(e){
+				currentSelection = -1;
+				//proposalList.empty();
+				params.onBlur();
+			});
+			
+			searchContainer.append(input);
+			searchContainer.append(proposals);		
+			
+			if(params.showButton){
+				//Search button
+				var button = $('<div></div>')
+					.addClass('autocomplete-button')
+					.html(params.buttonText)
+					.css({
+						'height': params.height + 2,
+						'line-height': params.height + 'px'
+					})
+					.click(function(){
+						proposalList.empty();
+						/*
+						* zhuqr�޸�
+						* params.onSubmit(input.val());
+						* */
+						params.onSubmit(input);
+					});
+				searchContainer.append(button);	
+			}
+	
+			$(this).append(searchContainer);	
+			
+			if(params.showButton){
+				//Width fix
+				searchContainer.css('width', params.width + button.width() + 50);
+			}
+		});
+
+		return this;
+	};
+
+})(jQuery);
