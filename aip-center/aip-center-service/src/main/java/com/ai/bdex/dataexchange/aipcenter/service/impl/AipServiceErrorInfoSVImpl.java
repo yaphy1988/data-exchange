5a61b20f3865e74f51a21e34a0144c7f4ec3b62c
Longsp <longsp@asiainfo.com>
2017-04-26 19:51:46 +0800

    give bean name

--- aip-center/aip-center-service/src/main/java/com/ai/bdex/dataexchange/aipcenter/service/impl/AipServiceErrorInfoSVImpl.java
+++ aip-center/aip-center-service/src/main/java/com/ai/bdex/dataexchange/aipcenter/service/impl/AipServiceErrorInfoSVImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ai.bdex.dataexchange.aipcenter.dao.mapper.AipServiceErrorInfoMapper;
 import com.ai.bdex.dataexchange.aipcenter.dao.model.AipServiceErrorInfo;
--- aip-center/aip-center-service/src/main/java/com/ai/bdex/dataexchange/aipcenter/service/impl/AipServiceOutParaSVImpl.java
+++ aip-center/aip-center-service/src/main/java/com/ai/bdex/dataexchange/aipcenter/service/impl/AipServiceOutParaSVImpl.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ai.bdex.dataexchange.aipcenter.dao.mapper.AipServiceOutParaMapper;
-import com.ai.bdex.dataexchange.aipcenter.dao.model.AipServiceOutParaExample;
 import com.ai.bdex.dataexchange.aipcenter.dao.model.AipServiceOutPara;
+import com.ai.bdex.dataexchange.aipcenter.dao.model.AipServiceOutParaExample;
 import com.ai.bdex.dataexchange.aipcenter.service.interfaces.IAipServiceOutParaSV;
 import org.springframework.stereotype.Service;
 
