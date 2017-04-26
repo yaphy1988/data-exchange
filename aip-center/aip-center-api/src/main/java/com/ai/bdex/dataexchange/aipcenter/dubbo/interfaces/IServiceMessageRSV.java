48657bf25a8d9c21c88b00c154ddf0dad7d03e5c
Longsp <longsp@asiainfo.com>
2017-04-26 09:43:02 +0800

    dto增加属性和注释

--- aip-center/aip-center-api/src/main/java/com/ai/bdex/dataexchange/aipcenter/dubbo/interfaces/IServiceMessageRSV.java
+++ aip-center/aip-center-api/src/main/java/com/ai/bdex/dataexchange/aipcenter/dubbo/interfaces/IServiceMessageRSV.java
@@ -3,5 +3,12 @@ package com.ai.bdex.dataexchange.aipcenter.dubbo.interfaces;
 import com.ai.bdex.dataexchange.aipcenter.dubbo.dto.ServiceDTO;
 
 public interface IServiceMessageRSV {
+	/**
+	 * 查询某个有效服务及与之有关其他的信息 ，serviceId 与serviceVersion构成主键
+	 * @param serviceId 服务ID
+	 * @param serviceVersion 服务版本
+	 * @return
+	 * @throws Exception
+	 */
 	ServiceDTO getServiceMessage(String serviceId,String serviceVersion)throws Exception;
 }
