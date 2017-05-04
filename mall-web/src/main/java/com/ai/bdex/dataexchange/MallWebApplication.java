ab62e500340ce8b43f37933e8120f4393feaad65
xiongqian <xiongqian@asiainfo.com>
2017-05-04 16:31:07 +0800

    增加system conf

--- mall-web/src/main/java/com/ai/bdex/dataexchange/MallWebApplication.java
+++ mall-web/src/main/java/com/ai/bdex/dataexchange/MallWebApplication.java
@@ -1,14 +1,24 @@
 package com.ai.bdex.dataexchange;
 
+ import com.ai.paas.config.ConfigurationCenter;
+ import com.ai.paas.config.SystemConfiguration;
+ import com.ai.paas.config.SystemConfigurationImpl;
+ import com.ai.paas.session.impl.SessionManager;
+ import com.ai.paas.util.SystemConfUtil;
+ import com.ai.paas.util.Utils;
  import com.alibaba.boot.dubbo.annotation.EnableDubboConfiguration;
 
 import org.slf4j.Logger; 
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
+ import org.springframework.beans.factory.annotation.Value;
+ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+ import org.springframework.context.ApplicationContext;
+ import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
+ import java.io.IOException;
+ import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,19 +26,12 @@ import java.util.List;
 @EnableDubboConfiguration
 public class MallWebApplication {
 
-	public static void main(String[] args) { 
-		SpringApplication.run(MallWebApplication.class, args);
+	@Value("${application.system.confpath:/com/ai/paas/system/conf}")
+	private String systemConfPath;
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(MallWebApplication.class, args);
+		Utils.setCtx(context);
 		System.out.println("--------------------恭喜你启动Mail-web成功了！！！-----------------");
 	}
-
-//	@Bean
-//	public FilterRegistrationBean filterRegistrationBean(){
-//		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-////		LoginAuthFilter authFilter = new LoginAuthFilter();
-////		filterRegistrationBean.setFilter(authFilter);
-//		List<String> urlPatterns = new ArrayList<>();
-//		urlPatterns.add("/**");
-//		filterRegistrationBean.setUrlPatterns(urlPatterns);
-//		return filterRegistrationBean;
-//	}
 }
