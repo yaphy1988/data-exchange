cb2923c67e0ed4d53b2fb9fbd58bd17900fc56e9
landeng <landeng@asiainfo.com>
2017-05-06 10:04:45 +0800

    购物车

--- mall-web/src/main/java/com/ai/bdex/dataexchange/busi/order/controller/OrderController.java
+++ mall-web/src/main/java/com/ai/bdex/dataexchange/busi/order/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.ai.bdex.dataexchange.busi.order.controller;
 
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
@@ -16,10 +18,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.gds.GdsSkuReqDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.gds.GdsSkuRespDTO;
 import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.page.DataCustomizationRespDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.gds.IGdsInfoRSV;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.gds.IGdsSkuRSV;
 import com.ai.bdex.dataexchange.util.StaffUtil;
 import com.ai.paas.util.CacheUtil;
+import com.ai.paas.utils.CollectionUtil;
 import com.ai.paas.utils.StringUtil;
+import com.alibaba.boot.dubbo.annotation.DubboConsumer;
 
 /**
  * 
@@ -27,8 +35,10 @@ import com.ai.paas.utils.StringUtil;
  * Date: 2017年4月19日 <br>
  * Copyright (c) 2017 <br>
  * 
- * @author liangwy
+ * @author landeng
  */
+
+
 @Controller
 @RequestMapping(value = "/order")
 public class OrderController {
@@ -37,33 +47,53 @@ public class OrderController {
 	private final static String CUSTOMDATA_STATUS_VALID = "1";// 有效
 	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 	
+	@DubboConsumer(timeout = 30000)
+	IGdsInfoRSV iGdsInfoRSV;
+	
+	@DubboConsumer(timeout = 30000)
+	IGdsSkuRSV iGdsSkuRSV;
+	
     //预定的数据展现
 	@RequestMapping(value = "/gdstmpsavesession")
 	public ModelAndView saveTosession(Model model, HttpServletRequest request) {
 		//取用户ID
 		HttpSession hpptsesion = request.getSession(); 
-		String staff_id = StaffUtil.getStaffId(hpptsesion);
-		
+		String staff_id = StaffUtil.getStaffId(hpptsesion); 
 		//将商品的名称，gdsID，套餐id，skuID，带过来，存储到session中。价格必须从商品服务从新回去
         //商品名称和ID
-		String gdsid =  request.getParameter("gdsid");
-		String gdsname =  request.getParameter("gdsname");
-		
+		int gdsid = Integer.parseInt(request.getParameter("gdsid"));
+		String gdsname =  request.getParameter("gdsname"); 
 		//套餐信息
-		String skusid =  request.getParameter("gdsid");
-		String skuname =  request.getParameter("gdsname");
-		
-		//获取商品的价格和图片
-		long gdsprice = 1;//单位分
+		int skusid =  Integer.parseInt(request.getParameter("skuid"));
+		String skuname =  request.getParameter("skuname"); 
+		//获取商品的价格和图
+		GdsSkuRespDTO gdsSkuRespDTO = new GdsSkuRespDTO(); 
+		GdsSkuReqDTO dsSkuReqDTO = new GdsSkuReqDTO();
+		dsSkuReqDTO.setGdsId(gdsid);
+		dsSkuReqDTO.setSkuId(skusid);
+		dsSkuReqDTO.setStatus(STATUS_VALID);
+		List<GdsSkuRespDTO>  listGdsSku =new ArrayList<>();
+		try { 
+			listGdsSku = iGdsSkuRSV.queryGdsSkuList(dsSkuReqDTO);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+		if(!CollectionUtil.isEmpty(listGdsSku) )
+		{
+			gdsSkuRespDTO = listGdsSku.get(0);
+		}
+	   /*	long gdsprice = 1;//单位分
 		String vsfid = "";//图片ID 
-        
+       */        
 		Object obj = new Object();
 		//每次进来都是讲session赋值为新的数据
-		CacheUtil.addItem(staff_id, obj);
+		// CacheUtil.addItem(staff_id, gdsSkuRespDTO);
+	    request.setAttribute("skuInfo",gdsSkuRespDTO);
+	    request.setAttribute("gdsname",gdsname);
+	    request.setAttribute("skuname",skuname);
 		//返回预购界面
-		ModelAndView modelAndView = new ModelAndView("index");
-		return modelAndView;
-		
+		ModelAndView modelAndView = new ModelAndView("shoppint_cart");
+		return modelAndView; 
 	}
 	  //修改预定的数据的数量，增加或者减少
 	@RequestMapping(value = "/updategdstmpsave")
