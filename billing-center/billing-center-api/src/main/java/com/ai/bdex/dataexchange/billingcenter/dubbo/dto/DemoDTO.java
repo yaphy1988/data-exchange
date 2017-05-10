4afe8da0cdb865ed164775c5dc97a099ec5b566f
zhaoyf7 <zhaoyf7@asiainfo.com>
2017-05-10 18:21:38 +0800

    增加spring-aop声明式事务控制

--- billing-center/billing-center-api/src/main/java/com/ai/bdex/dataexchange/billingcenter/dubbo/dto/DemoDTO.java
+++
@@ -1,7 +0,0 @@
-package com.ai.bdex.dataexchange.billingcenter.dubbo.dto;
-
-/**
- * Created by fangyunfeng on 2017/4/12.
- */
-public class DemoDTO {
-}
--- billing-center/billing-center-service/src/main/java/com/ai/bdex/dataexchange/billingcenter/dao/model/DemoModelCriteria.java
+++
@@ -1,7 +0,0 @@
-package com.ai.bdex.dataexchange.billingcenter.dao.model;
-
-/**
- * Created by fangyunfeng on 2017/4/12.
- */
-public class DemoModelCriteria {
-}
