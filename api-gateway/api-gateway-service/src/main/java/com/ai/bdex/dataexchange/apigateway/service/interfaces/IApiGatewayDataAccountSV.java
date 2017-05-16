f175ad80ea29b5280febb318dedd2cb7e9e661a6
清凉微风 <514630382@qq.com>
2017-05-16 15:56:23 +0800

    扣费接口注释

--- api-gateway/api-gateway-service/src/main/java/com/ai/bdex/dataexchange/apigateway/service/interfaces/IApiGatewayDataAccountSV.java
+++ api-gateway/api-gateway-service/src/main/java/com/ai/bdex/dataexchange/apigateway/service/interfaces/IApiGatewayDataAccountSV.java
@@ -11,13 +11,15 @@ public interface IApiGatewayDataAccountSV {
     /**
      * 扣费接口
      * 必填字段如下：
-     * invokeSeq
-     * userId
-     * realServiceId
-     * consumeNum
-     * consumeMoney
+     * invokeSeq 调用流水
+     * userId 用户id
+     * realServiceId 接口id
+     * consumeNum - 消耗次数，填写 1
+     * consumeMoney - 消耗金额，填写接口调用单价
      * @param consumeDTO
      * @return OK-成功，NA-没有权限访问
+     * consumeDTO.result 扣费结果：OK-扣费成功，NA-余额不足或找不到记录
+     * consumeDTO.billId 扣费成功后返回的账单id
      * @throws BusinessException
      */
     DataConsumeRespDTO dealDataCharge(DataConsumeDTO consumeDTO) throws BusinessException;
