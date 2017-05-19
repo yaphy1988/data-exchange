698bbc2959510b2e267ec365ba3ef84c2566fe57
gongxq <gongxq@asiainfo.com>
2017-05-19 16:37:26 +0800

    job

---
+++ trade-center/trade-center-servie/src/main/java/com/ai/bdex/dataexchange/tradecenter/SolrDeltaImportClearJob.java
@@ -0,0 +1,23 @@
+package com.ai.bdex.dataexchange.tradecenter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+
+import com.ai.bdex.dataexchange.exception.BusinessException;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.solr.IDeltaIndexServiceRSV;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.solr.SolrCoreEnum;
+import com.ai.paas.util.Utils;
+
+public class SolrDeltaImportClearJob {
+    private static Logger logger = LoggerFactory.getLogger(SolrDeltaImportClearJob.class);
+    public static void main(String[] args){
+        try {
+             Utils.getCtx().getBean("deltaIndexServiceRSV",IDeltaIndexServiceRSV.class).deltaFullImport(SolrCoreEnum.GDS.getCode(), true);
+             logger.error("====================================重建索引成功！====================================");
+         } catch (BeansException | BusinessException e) {
+             logger.error("重建索引失败！",e);
+         }
+     }
+}
+
