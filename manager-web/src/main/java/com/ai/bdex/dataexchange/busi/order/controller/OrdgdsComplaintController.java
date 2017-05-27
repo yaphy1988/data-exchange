5d3f77ddf11fff07cf0217f58da99b231caf9d85
liangwy <liangwy@asiainfo.com>
2017-05-27 15:20:54 +0800

    我的投诉

--- manager-web/src/main/java/com/ai/bdex/dataexchange/busi/order/controller/OrdgdsComplaintController.java
+++ manager-web/src/main/java/com/ai/bdex/dataexchange/busi/order/controller/OrdgdsComplaintController.java
@@ -1,7 +1,23 @@
 package com.ai.bdex.dataexchange.busi.order.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import com.ai.bdex.dataexchange.aipcenter.dubbo.dto.DataAccountDTO;
+import com.ai.bdex.dataexchange.aipcenter.dubbo.interfaces.IAipCenterDataAccountRSV;
+import com.ai.bdex.dataexchange.busi.order.entity.OrdMainInfoVO;
+import com.ai.bdex.dataexchange.constants.Constants;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.gds.GdsInfoReqDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.gds.GdsInfoRespDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.order.OrdInfoReqDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.order.OrdInfoRespDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.order.OrdMainInfoReqDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.dto.order.OrdMainInfoRespDTO;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.gds.IGdsInfoRSV;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.order.IOrderInfoRSV;
+import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.order.IOrderMainInfoRSV;
+import com.ai.bdex.dataexchange.util.StaffUtil;
+import com.alibaba.dubbo.common.utils.CollectionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -16,6 +32,10 @@ import com.ai.bdex.dataexchange.tradecenter.dubbo.interfaces.complaint.IOrdCompl
 import com.ai.bdex.dataexchange.util.StringUtil;
 import com.alibaba.boot.dubbo.annotation.DubboConsumer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /**
  * 订单商品投诉
  * Description: <br>
@@ -29,9 +49,19 @@ import com.alibaba.boot.dubbo.annotation.DubboConsumer;
 public class OrdgdsComplaintController {
 	@DubboConsumer(timeout=3000)
 	private IOrdComplaintRSV iOrdComplaintRSV;
-	
+	@DubboConsumer(timeout = 300000)
+	private IOrderInfoRSV iOrderInfoRSV;
+	@DubboConsumer(timeout = 300000)
+	private IOrderMainInfoRSV iOrderMainInfoRSV;
+	@DubboConsumer
+	private IAipCenterDataAccountRSV iAipCenterDataAccountRSV;
+	@DubboConsumer(timeout = 30000)
+	IGdsInfoRSV iGdsInfoRSV;
 	private final Log logger = LogFactory.getLog(getClass());
-	
+	private final static Integer PAGE_SIZE = 10;//页数
+	private final static String TMPUSERID = "tmpuser";// 临时用户
+
+
 	@RequestMapping(value="/pageInit")
 	public ModelAndView pageInit(){
 		ModelAndView modelAndView = new ModelAndView("goods_complaint");
@@ -62,5 +92,100 @@ public class OrdgdsComplaintController {
 			logger.error("[查询我的订单投诉异常]，异常信息："+e.getMessage());
 		}
 		return "goods_complaint :: #tab02";
+	}
+
+	/**
+	 * 查询我的订单List
+	 *
+	 * @param model
+	 * @param
+	 * @return
+	 */
+	@RequestMapping(value = "/myOrderList")
+	public String myOrderList(Model model, OrdMainInfoVO ordMainInfoVO, HttpServletRequest request ) {
+		try {
+			PageResponseDTO<OrdMainInfoRespDTO> pageInfo = new PageResponseDTO<OrdMainInfoRespDTO>();
+			OrdMainInfoReqDTO ordMainReqDTO = new OrdMainInfoReqDTO();
+			ordMainReqDTO.setPageNo(ordMainInfoVO.getPageNo());
+			ordMainReqDTO.setPageSize(PAGE_SIZE);
+			HttpSession hpptsesion = request.getSession();
+			String staff_id = StaffUtil.getStaffId(hpptsesion);
+			if(StringUtil.isBlank(staff_id)){
+				staff_id = TMPUSERID;
+			}
+			ordMainReqDTO.setStaffId(staff_id);
+			//不是30的数据要查询出来
+			List<String> listOrderType = new ArrayList<String>();
+			listOrderType.add(Constants.Order.ORDER_TYPE_10);
+			listOrderType.add(Constants.Order.ORDER_TYPE_20);
+			ordMainReqDTO.setordertypeList(listOrderType)  ;
+			pageInfo = iOrderMainInfoRSV.queryOrdMainInfoPage(ordMainReqDTO);
+			if(!CollectionUtils.isEmpty(pageInfo.getResult())){
+				for(OrdMainInfoRespDTO ordMainRespDTO :pageInfo.getResult()){
+					OrdInfoReqDTO ordInfoReqDTO = new OrdInfoReqDTO();
+					ordInfoReqDTO.setOrderId(ordMainRespDTO.getOrderId());
+					List<OrdInfoRespDTO> ordInfoList = iOrderInfoRSV.queryOrderInfoList(ordInfoReqDTO);
+					OrdInfoRespDTO ordInfoRespDTO = new OrdInfoRespDTO();
+					if(CollectionUtils.isNotEmpty(ordInfoList)){
+						//一个订单只有一个子订单
+						ordInfoRespDTO=ordInfoList.get(0);
+						//从计费接口去可用次数
+						DataAccountDTO dataAccountDTO= iAipCenterDataAccountRSV.queryDataAccountBySubOrder(ordInfoRespDTO.getSubOrder());
+						if(dataAccountDTO!=null&&dataAccountDTO.getLeftNum()!=null){
+							ordInfoRespDTO.setLeftCount(dataAccountDTO.getLeftNum());
+						}
+					}
+					ordMainRespDTO.setOrdInfoRespDTO(ordInfoRespDTO);
+				}
+			}
+			model.addAttribute("pageInfo", pageInfo);
+		} catch (Exception e) {
+			logger.error("查询我的订单列表失败！原因是：" + e.getMessage());
+		}
+		return "goods_complaint :: #tab01";
+	}
+	@RequestMapping(value = "/iwantComplaint")
+	public String iwantComplaint(HttpServletRequest request,Model model){
+		String orderId = request.getParameter("orderId");
+		String gdsName="";
+		try{
+			OrdInfoReqDTO ordInfoReqDTO = new OrdInfoReqDTO();
+			ordInfoReqDTO.setOrderId(orderId);
+			List<OrdInfoRespDTO> ordInfoList = iOrderInfoRSV.queryOrderInfoList(ordInfoReqDTO);
+			OrdInfoRespDTO ordInfo = ordInfoList.get(0);
+			if (ordInfo != null){
+				//通过商品id去搜索商品信息，获取到商品的图片ID
+				GdsInfoReqDTO gdsInfoReqDTO = new GdsInfoReqDTO();
+				GdsInfoRespDTO gdsInfoRespDTO = new GdsInfoRespDTO();
+				gdsInfoReqDTO.setGdsId(ordInfo.getGdsId());
+				gdsInfoRespDTO =  iGdsInfoRSV.queryGdsInfo(gdsInfoReqDTO);
+				gdsName = gdsInfoRespDTO.getGdsName();
+			}
+			model.addAttribute("gdsName",gdsName);
+			model.addAttribute("orderId",orderId);
+		}catch (Exception e){
+			logger.error("根据订单id查询订单信息失败：" + e.getMessage());
+		}
+		return "goods_complaint :: #modal_content";
+	}
+	@RequestMapping(value = "/modalDataSubmit")
+	public Map<String, Object> modalDataSubmit(Model model,HttpServletRequest request){
+		String orderId = request.getParameter("orderId");
+		String complaintItem = request.getParameter("complaintItem");
+		String complaintContent = request.getParameter("complaintContent");
+		String linPerson = request.getParameter("linPerson");
+		String linkPhone = request.getParameter("linkPhone");
+		String email = request.getParameter("email");
+		Map<String,Object> rMap = new HashMap<String,Object>();
+		try {
+			OrdComplaintReqDTO ordComplaintReqDTO = new OrdComplaintReqDTO();
+			ordComplaintReqDTO.setOrderId(orderId);
+			ordComplaintReqDTO.setComplaintItem(complaintItem);
+//			iOrdComplaintRSV.insertOrdComplaint();
+
+		}catch (Exception e){
+			logger.error("投诉信息新增/编辑失败：" + e.getMessage());
+		}
+		return rMap;
 	}
 }
--- manager-web/src/main/resources/static/js-busi/complaint/goods_complaint.js
+++ manager-web/src/main/resources/static/js-busi/complaint/goods_complaint.js
@@ -1,9 +1,99 @@
 $(function(){
-	queryComplaintList();
+    myOrderList(1);
+
+    $('.nav.nav-tabs>li').on('click',function(u){
+        // $('.nav.nav-tabs>li').removeClass('active');
+        // $(this).attr('class','active');
+		if ($(this).children().attr('href')=='#tab01'){
+            myOrderList(1);
+		}else{
+            queryComplaintList(1);
+		}
+	});
+
+
 });
-function queryComplaintList(){
+//数据校验
+function checkModalData(params){
+
+
+    if(!params.orderId){
+        WEB.msg.info('提示','订单编号不能为空！');
+        return false;
+    }
+    if(!params.complaintContent){
+        $('#complaintContent').next().attr('style','visibility: block;');
+        return false;
+    }
+    if(!params.lnkposen){
+        $('#lnkposen').next().attr('style','visibility: block;');
+        return false;
+    }
+    if(!WEB.check.isPhone(params.linkPhone)){
+        $('#lnkphone').next().attr('style','visibility: block;');
+        return false;
+
+    }
+    if(!WEB.check.isEmail(params.email)){
+        $('#lnkemail').next().attr('style','visibility: block;');
+        return false;
+    }
+    return true;
+}
+//更新、新增
+function modalDataSubmit(){
+    var orderId =$('#my_orderId').val();
+    var complaintContent = $('#complaintContent').val();
+    var linPerson = $('#lnkposen').val();
+    var linkPhone = $('#lnkphone').val();
+    var email = $('#lnkemail').val();
+    var params ={
+        orderId:orderId,
+        complaintContent:complaintContent,
+        linPerson:linPerson,
+        linkPhone:linkPhone,
+        email:email
+    }
+    var chekcFlag = checkModalData(params);
+    var url = MANAGE_ROOT+'/ordgdsComplaint/modalDataSubmit';
+    $.ajax({
+        url:url,
+        dataType:'json',
+        data:params,
+        cache:false,
+        async:true,
+        success:function (data) {
+            if(data.success){
+
+            }else{
+                WEB.msg.info('提示','操作失败！');
+            }
+        }
+    })
+}
+
+function modalDatahiden() {
+    $('#myModal').modal('hide');
+}
+//我要投诉
+function iwantComplaint(orderId){
+    $.ajax({
+        url:MANAGE_ROOT+'/ordgdsComplaint/iwantComplaint',
+        data:{orderId:orderId},
+        dataType:'html',
+        cache:false,
+        async:true,
+        success:function (data) {
+			$('#myModal>div').html(data);
+            $('#myModal').modal('show');
+        }
+    })
+
+}
+
+function queryComplaintList(index){
 	var url = MANAGE_ROOT+'/ordgdsComplaint/queryComplaint';
-	var params={};
+	var params={pageNo:index};
 	$.ajax({
 		url:url,
 		data:params,
@@ -12,6 +102,28 @@ function queryComplaintList(){
 		async:true,
 		success:function(data){
 			$('#complant_content').html(data);
+			$('#complant_content div').removeClass('active');
+			$('#tab02').attr('class','tab-pane active');
+            $('.nav.nav-tabs>li').removeClass('active');
+            $('.nav.nav-tabs>li').eq(1).attr('class','active');
 		}
 	});
+}
+
+function myOrderList(index){
+    var param={pageNo:index};
+    $.ajax({
+        url:MANAGE_ROOT+'/ordgdsComplaint/myOrderList',
+        cache:false,
+        async:true,
+        dataType:'html',
+        data : param,
+        success:function(data){
+            $('#complant_content').html(data);
+            $('#complant_content div').removeClass('active');
+            $('#tab01').attr('class','tab-pane active');
+            $('.nav.nav-tabs>li').removeClass('active');
+            $('.nav.nav-tabs>li').eq(0).attr('class','active');
+        }
+    });
 }
