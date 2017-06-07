3711d797ef7f8c85cf24a467b54425f451749402
liangwy <liangwy@asiainfo.com>
2017-06-07 11:52:03 +0800

    报表统计：支持一个页面多个table

--- manager-web/src/main/resources/static/js-busi/report/report-frame.js
+++ manager-web/src/main/resources/static/js-busi/report/report-frame.js
@@ -1,15 +1,11 @@
 /**
  * Created by yaphy on 2017/5/24.
  */
-var $table ,
-    $remove ,
-    selections = [];
-function initTable() {
-        $table = $('#table'),
-        $remove = $('#remove'),
+function initTable(tableId) {
+    var $table = $('#'+tableId+'');
     $table.bootstrapTable({
         height: getHeight(),
-        queryParams : queryParams,
+        // queryParams : queryParams,
         pageList: [10, 25, 50, 100,200],
         ajaxOptions:{
             crossDomain: true,
@@ -17,8 +13,6 @@ function initTable() {
                 withCredentials: true
             }
         },
-        clickToSelect:true,
-        showExport: true,                     //是否显示导出
         exportDataType: "basic",              //basic', 'all', 'selected'.
         responseHandler: responseHandler,
 
@@ -32,42 +26,14 @@ function initTable() {
     setTimeout(function () {
         $table.bootstrapTable('resetView');
     }, 200);
-    $table.on('check.bs.table uncheck.bs.table ' +
-        'check-all.bs.table uncheck-all.bs.table', function () {
-        $remove.prop('disabled', !$table.bootstrapTable('getSelections').length);
-        // save your data, here just save the current page
-        selections = getIdSelections();
-        // push or splice the selections if you want to save all data selections
-    });
-/*    $table.on('expand-row.bs.table', function (e, index, row, $detail) {
-        if (index % 2 == 1) {
-            $detail.html('Loading from ajax request...');
-            $.get('LICENSE', function (res) {
-                $detail.html(res.replace(/\n/g, '<br>'));
-            });
-        }
-    });*/
     $table.on('all.bs.table', function (e, name, args) {
         console.log(name, args);
     });
-    $remove.click(function () {
-        var ids = getIdSelections();
-        $table.bootstrapTable('remove', {
-            field: 'id',
-            values: ids
-        });
-        $remove.prop('disabled', true);
-    });
-    $(window).resize(function () {
+/*    $(window).resize(function () {
         $table.bootstrapTable('resetView', {
             height: getHeight()
         });
-    });
-}
-function getIdSelections() {
-    return $.map($table.bootstrapTable('getSelections'), function (row) {
-        return row.id
-    });
+    });*/
 }
 
 function detailFormatter(index, row) {
@@ -77,40 +43,6 @@ function detailFormatter(index, row) {
     });
     return html.join('');
 }
-function operateFormatter(value, row, index) {
-    return [
-        '<a class="like" href="javascript:void(0)" title="Like">',
-        '<i class="glyphicon glyphicon-heart"></i>',
-        '</a>  ',
-        '<a class="remove" href="javascript:void(0)" title="Remove">',
-        '<i class="glyphicon glyphicon-remove"></i>',
-        '</a>'
-    ].join('');
-}
-window.operateEvents = {
-    'click .like': function (e, value, row, index) {
-        alert('You click like action, row: ' + JSON.stringify(row));
-    },
-    'click .remove': function (e, value, row, index) {
-        $table.bootstrapTable('remove', {
-            field: 'id',
-            values: [row.id]
-        });
-    }
-};
-function totalTextFormatter(data) {
-    return 'Total';
-}
-function totalNameFormatter(data) {
-    return data.length;
-}
-function totalPriceFormatter(data) {
-    var total = 0;
-    $.each(data, function (i, row) {
-        total += +(row.price.substring(1));
-    });
-    return '$' + total;
-}
 function getHeight() {
     return $(window).height() - $('h1').outerHeight(true);
 }
@@ -119,10 +51,6 @@ function getHeight() {
 //参数：res为从服务器请求到的数据。服务器返回PageInfo<T>的json格式，必须包含total
 function responseHandler(res) {
     if (res) {
-        var rows = res.list;
-        $.each(rows, function (i, row) {
-            row.state = $.inArray(row.id, selections) !== -1;
-        });
         return {
             "rows" : res.list,
             "total" : res.total
